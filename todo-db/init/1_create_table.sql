4f8262dd84778af242b6f809f03e6d8396f6c866
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-11-09 02:07:17 +0900

    categoriesテーブル追加

--- todo-db/init/1_create_table.sql
+++ todo-db/init/1_create_table.sql
@@ -11,3 +11,13 @@ CREATE TABLE IF NOT EXISTS todos (
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE todos ADD category_id int NOT NULL;
+ALTER TABLE todos ADD FOREIGN KEY (category_id) REFERENCES categories(id);
--- todo-db/init/3_insert_data.sql
+++ todo-db/init/3_insert_data.sql
@@ -1,5 +1,13 @@
 use tododb;
 
-INSERT INTO todos (title, content) VALUES ("Hello", "hello world!");
-INSERT INTO todos (title, content) VALUES ("Test", "testtest");
-INSERT INTO todos (title, content) VALUES ("programming", "sinatra server");
+INSERT INTO categories (title) VALUES ("none");
+INSERT INTO categories (title) VALUES ("study");
+INSERT INTO categories (title) VALUES ("programming");
+INSERT INTO categories (title) VALUES ("work");
+INSERT INTO categories (title) VALUES ("life");
+
+INSERT INTO todos (title, content, category_id) VALUES ("AI", "no.5-homework", 2);
+INSERT INTO todos (title, content, category_id) VALUES ("買い物", "牛乳・トマト・パスタ・食パン", 5);
+INSERT INTO todos (title, content, category_id) VALUES ("programming", "sinatra server", 3);
+INSERT INTO todos (title, content, category_id) VALUES ("面接", "x月y日zzz社", 4);
+INSERT INTO todos (title, content, category_id) VALUES ("ブログ", "3記事執筆", 1);
